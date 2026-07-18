9108b4424df00512cbb226e649b2df17e523d332
Antonio Sifuentes <20236823@aloe.ulima.edu.pe>
2026-07-18 04:07:08 -0500

    zzzz

--- src/api/base.js
+++ src/api/base.js
@@ -3,43 +3,23 @@ import axios from 'axios'
 const URI = 'http://localhost:3005'
 
 const get = async (endpoint) => {
-    try {
-        const url = URI.concat(endpoint)
-        return await axios.get(url)
-    } catch (error) {
-        console.error(error)
-        return null
-    }
+    const url = URI.concat(endpoint)
+    return await axios.get(url)
 }
 
 const post = async (endpoint, request) => {
-    try {
-        const url = URI.concat(endpoint)
-        return await axios.post(url, request)
-    } catch (error) {
-        console.error(error)
-        return null
-    }
+    const url = URI.concat(endpoint)
+    return await axios.post(url, request)
 }
 
 const put = async (endpoint, request) => {
-    try {
-        const url = URI.concat(endpoint)
-        return await axios.put(url, request)
-    } catch (error) {
-        console.error(error)
-        return null
-    }
+    const url = URI.concat(endpoint)
+    return await axios.put(url, request)
 }
 
 const remove = async (endpoint) => {
-    try {
-        const url = URI.concat(endpoint)
-        return await axios.delete(url)
-    } catch (error) {
-        console.error(error)
-        return null
-    }
+    const url = URI.concat(endpoint)
+    return await axios.delete(url)
 }
 
 const Base = {
